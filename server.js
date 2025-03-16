28414de93333998b29354a5a4a5d8f02c5ee964b
Karem Oropeza <karemoropezayan@gmail.com>
2025-03-16 15:27:25 -0600

    Hash Ssync

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
     console.log(hash);
     bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
     console.log(res);
@@ -27,7 +27,11 @@ const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 //END_ASYNC
 
 //START_SYNC
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
 
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 
 //END_SYNC
